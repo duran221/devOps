1d4631b25365d2867060bc4c45db434d5e07ef9e
Alvaro Jose <aljoroga11@gamil.com>
2025-04-03 20:30:38 -0500

    add comentario

--- bienvenido.php
+++ bienvenido.php
@@ -23,7 +23,7 @@
       </div>
     </div>
   </div>
-
+// mi primer comentario
   <!-- Agregar el enlace al script de Bootstrap (requerido para algunos componentes interactivos) -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>

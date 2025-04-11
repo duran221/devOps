4515e43a5801e2d9538a24f87d1fe56d1247b139
Manuel Madrigal Medina <manuelmadrigal1303@gmail.com>
2025-04-10 20:34:32 -0500

    changed the title

--- bienvenido.php
+++ bienvenido.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Bienvenido</title>
+  <title>Bienvenidos todos</title>
   <!-- Agregar los enlaces a los estilos de Bootstrap -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
+
 <body>
   <div class="container mt-5">
+  
     <div class="row justify-content-center">
       <div class="col-md-6">
         <div class="card">
@@ -30,6 +33,7 @@
   <!-- Agregar el enlace al script de Bootstrap (requerido para algunos componentes interactivos) -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
 
 <!--//' OR '1'='1-->

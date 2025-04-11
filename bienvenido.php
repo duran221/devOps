49177fc55444ba7fd4d0701f8cf4bc05a3176eb4
Manuel Madrigal Medina <manuelmadrigal1303@gmail.com>
2025-04-10 20:37:33 -0500

    changed the title2

--- bienvenido.php
+++ bienvenido.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Bienvenidos todos</title>
+  <title>Bienvenidos </title>
   <!-- Agregar los enlaces a los estilos de Bootstrap -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
